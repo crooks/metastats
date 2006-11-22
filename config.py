f166d12d5d67f7a911e8c782f83f5faa8f0dedd8
crooks <crooks@e883b274-e99e-40cc-a60d-0021bd3fd338>
2006-11-22 17:22:03 +0000

    Added template files for config.py and metastats.py.  Later on, config.py will hold the config and metastats.py will be the executable containing __main__

---
+++ config.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+#
+# vim: tabstop=4 expandtab shiftwidth=4 noautoindent
+#
+# config.py -- This is the config file for metastats
+#
+# Copyright (C) 2006 Steve Crook <steve@mixmin.org>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by the
+# Free Software Foundation; either version 2, or (at your option) any later
+# version.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTIBILITY
+# or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+# for more details.
+
+# This entry is the default added to the Path header.  If the message
+# doesn't contain a Message-ID, this entry is used for the right side
+# of that too.
