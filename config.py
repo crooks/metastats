0ef6f568ee8a9ffa64780e7ef4434b9b1b0ce05d
crooks <crooks@e883b274-e99e-40cc-a60d-0021bd3fd338>
2006-11-22 17:53:21 +0000

    Config file populated with basic settings from stats.py. Config will no longer be held in a dictionary but in individual variables.

--- config.py
+++ config.py
@@ -19,3 +19,27 @@
 # This entry is the default added to the Path header.  If the message
 # doesn't contain a Message-ID, this entry is used for the right side
 # of that too.
+
+# Directory where this program resides
+basedir = "/home/crooks/testing"
+
+# Base URL for access to stats
+baseurl = "http://stats.mixmin.org"
+
+# Name of index file
+index_file = "index.html"
+
+# Number of hours old a timestamp can be and a pinger
+# entry still considered active.
+active_age = 8
+
+# How many hours ahead of UTC is considered valid.
+active_future =  2
+
+# This is the filename used to create a .txt and .html report file
+# for the remailer genealogy section.
+gene_report_name = "genealogy"
+
+# Socket timeout, used to prevent url retrieval from hanging.
+# Value is the number of seconds to wait for a url to respond.
+timeout = 30
